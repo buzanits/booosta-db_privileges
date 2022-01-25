edd6a8969a07d03d6543196ee5d3d6bfdff8afcc
Peter Buzanits <p.buzanits@epb.at>
2022-01-25 17:23:04 +0100

    fix paths

--- exec/admin_role.php
+++ exec/admin_role.php
@@ -1,7 +1,7 @@
 <?php
 namespace booosta\db_privileges;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../../../vendor/autoload.php';
 
 use booosta\Framework as b;
 b::croot();
